92e278a4cd8ea4a005219dfbe0149c79a773b68e
romerodevelopments <gandresrp@gmail.com>
2024-08-18 23:17:48 -0400

    feat: create user respository

--- app/src/main/java/com/example/recipesapp/data/UserRepository.kt
+++ app/src/main/java/com/example/recipesapp/data/UserRepository.kt
@@ -1,2 +1,25 @@
 package com.example.recipesapp.data
 
+import com.example.recipesapp.models.User
+
+object UserRepository {
+    private val users = mutableListOf(
+        User("user1", "user1@example.com", "password1"),
+        User("user2", "user2@example.com", "password2"),
+        User("user3", "user3@example.com", "password3"),
+        User("user4", "user4@example.com", "password4"),
+        User("user5", "user5@example.com", "password5")
+    )
+
+    fun addUser(username: String, email: String, password: String) {
+        users.add(User(username, email, password))
+    }
+
+    fun validateUser(email: String, password: String): Boolean {
+        return users.any { it.email == email && it.password == password }
+    }
+
+    fun getAllUsers(): List<User> {
+        return users
+    }
+}
