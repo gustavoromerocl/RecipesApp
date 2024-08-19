79527a6e6a1f367ef835c4a935fc2965108fd207
romerodevelopments <gandresrp@gmail.com>
2024-08-18 23:16:09 -0400

    feat: implement navigator

--- app/src/main/java/com/example/recipesapp/navigation/NavManager.kt
+++ app/src/main/java/com/example/recipesapp/navigation/NavManager.kt
@@ -1,1 +1,35 @@
-package com.example.recipesapp.navigation
+package com.example.navegacion.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.recipesapp.views.HomeView
+import com.example.recipesapp.views.LoginView
+import com.example.recipesapp.views.PasswordRecoveryView
+import com.example.recipesapp.views.RegisterView
+
+@Composable
+fun NavManager(){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "Login"  ){
+        composable("signIn"){
+            RegisterView(navController)
+        }
+
+        composable("login"){
+            LoginView(navController)
+        }
+
+        composable("passwordRecovery"){
+            PasswordRecoveryView(navController)
+        }
+
+        composable("home"){
+            HomeView(navController)
+        }
+
+    }
+}
