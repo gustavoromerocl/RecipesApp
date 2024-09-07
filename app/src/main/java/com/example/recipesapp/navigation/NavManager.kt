9cff572007b96b173f9b709027c9d1effd8b3566
gustavoromerocl <gandresrp@gmail.com>
2024-09-06 23:39:53 -0400

    chore: remove unused code

--- app/src/main/java/com/example/recipesapp/navigation/NavManager.kt
+++ app/src/main/java/com/example/recipesapp/navigation/NavManager.kt
@@ -1,11 +1,9 @@
 package com.example.navegacion.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.example.recipesapp.views.HomeView
 import com.example.recipesapp.views.LoginView
 import com.example.recipesapp.views.PasswordRecoveryView
