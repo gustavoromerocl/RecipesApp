0b484e60435e0b2d955db32673f17866873fd9c3
gustavoromerocl <gandresrp@gmail.com>
2024-10-06 12:22:14 -0300

    feat: add update username method on session manager

--- app/src/main/java/com/example/recipesapp/session/SessionManager.kt
+++ app/src/main/java/com/example/recipesapp/session/SessionManager.kt
@@ -14,6 +14,14 @@ object SessionManager {
         editor.apply()
     }
 
+    // Actualizar solo el username de la sesión
+    fun updateUsernameInSession(context: Context, newUsername: String) {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString(KEY_USERNAME, newUsername)  // Solo actualizamos el username
+        editor.apply()  // Aplicamos los cambios
+    }
+
     fun getUserSession(context: Context): Pair<String?, String?> {
         val sharedPreferences: SharedPreferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
         val email = sharedPreferences.getString(KEY_EMAIL, null)
